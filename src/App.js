a6db4f5362164e6d015c56cd5ee8ae964eddce11
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-03-12 15:00:40 +0000

    delete elements from other project & made basic stractures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
-import Header from "./components/header/Header";
-import Main from "./components/main/Main";
-import Info from "./components/info/Info";
-import Games from "./components/games/Games";
-import SocialToken from "./components/social_token/Social_Token";
-import Buy from "./components/buy/Buy";
-import Tokenomics from "./components/tokenomics/Tokenomics";
-import Roadmap from "./components/roadmap/Roadmap";
-import Footer from "./components/footer/Footer";
+
 import Game from "./components/Game/Game";
 import WalletInfo from "./components/walletinfo/WalletInfo";
 import React, { useMemo } from 'react';
@@ -39,20 +31,9 @@ function App() {
     <WalletProvider wallets={wallets} autoConnect={true}>
       <WalletModalProvider>
     <div className="App">
-
-      <Header />
-      <Main />
       {/* <WalletInfo/> */}
       <Game />
-      <Info />
-      <Games />
-      <SocialToken />
-      <Tokenomics/>
-      <Buy />
-      <Roadmap />
-      <Footer />
-
-      
+            
     </div>
     </WalletModalProvider>
             </WalletProvider>
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,11 +1,4 @@
 import "./game.css";
-import coinImg from "./../../img/coin.svg";
-
-import Lottie from "lottie-react";
-import AnimationBeer from "./AnimationBeer.json";
-import { useRef } from "react";
-
-
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
@@ -15,112 +8,17 @@ function Game() {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
 
-  const cap20perc = [110, 111];
-  const cap50perc = [140, 141];
-  const cap95perc = [180, 181];
-
-
-  const lottieRefs = [
-    useRef(null),
-    useRef(null),
-    useRef(null)
-  ];
-
-  const playAnimation = (index, startAnim) => {
-    if (lottieRefs[index].current) {
-      lottieRefs[index].current.playSegments([startAnim, 242], true);
-    }
-  };
 
   return (
     <section className="Game">
-      <div className="container">
-        <div className="game__connect">
-          <WalletMultiButton id='connect_phantom' className="game__connectbutton"></WalletMultiButton>
-        </div>
-        <div className="game__headerblock">
-          <h1>GAME</h1>
-          <h2>Pour beer, place bet and win SOL</h2>
-        </div>
-        <div className="game__slotsblock">
-          <div className="game__slot">
-            <h1>
-              3 SLOT - CASHBACK <span>🥈</span>
-            </h1>
-            <h2>50% BACK OF YOUR INVESTMENT</h2>
-          </div>
-          <div className="game__slot">
-            <h1>
-              2 SLOT - WINNER <span>🏆</span>
-            </h1>
-            <h2>100% OF SLOT 2 + 50% 0F SLOT 3 + 2</h2>
-          </div>
-          <div className="game__slot">
-            <h1>
-              1 SLOT - LOOSER <span>🤡</span>
-            </h1>
-            <h2>GAME OVER, TRY AGAIN</h2>
-          </div>
-        </div>
-        <div className="game__balance">
-          <h1>
-            <img src={coinImg} alt="coinImg" />
-            6029.01 SOL
-          </h1>
-        </div>
-        <div className="game__winners">
-
-          <div className="game__winner">
-            <div className="game__winners_icon first"><span>🥈</span></div>
-            <div className="game__winners_cap">
-              <div className="game__winners_capanime" onClick={() => playAnimation(0, cap20perc[0])}>
-                <Lottie animationData={AnimationBeer}
-                  lottieRef={lottieRefs[0]}
-                  loop={false}
-                  autoplay={false}
-                  initialSegment={cap20perc}
-                />
-              </div>
-            </div>
-            <div className="game__winners_place first_place" id="winners_place0"></div>
-            <div className="game__winners_points"><h1>543</h1></div>
-          </div>
-
-          <div className="game__winner">
-            <div className="game__winners_icon second"><span>🏆</span></div>
-            <div className="game__winners_cap">
-              <div className="game__winners_capanime" onClick={() => playAnimation(1, cap50perc[0])}>
-                <Lottie animationData={AnimationBeer}
-                  lottieRef={lottieRefs[1]}
-                  loop={false}
-                  autoplay={false}
-                  initialSegment={cap50perc}
-                />
-              </div>
-            </div>
-
-            <div className="game__winners_place second_place" id="winners_place1"></div>
-            <div className="game__winners_points"><h1>302</h1></div>
-          </div>
-
-          <div className="game__winner">
-            <div className="game__winners_icon third"><span>🤡</span></div>
-            <div className="game__winners_cap">
-              <div className="game__winners_capanime" onClick={() => playAnimation(2, cap95perc[0])}>
-                <Lottie animationData={AnimationBeer}
-                  lottieRef={lottieRefs[2]}
-                  loop={false}
-                  autoplay={false}
-                  initialSegment={cap95perc}
-                />
-              </div>
-            </div>
-            <div className="game__winners_place third_place" id="winners_place2"></div>
-            <div className="game__winners_points"><h1>152</h1></div>
-          </div>
-
-        </div>
-
+      <div className="connect">
+      <WalletMultiButton></WalletMultiButton>
+      <div className="transactions">
+        <button>купить стол за usdt</button>
+        <button>купить стол за spark</button>
+        <button>купить стул за usdt</button>
+        <button>купить стул за spark</button>
+      </div>
       </div>
     </section>
   );
