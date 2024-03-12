b46c2f3e9b4f7d58e920815b222bfb1af295bc68
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-03-12 15:36:36 +0000

    lil changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { useMemo } from 'react';
 
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
-    PhantomWalletAdapter,
+  PhantomWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
@@ -15,29 +15,29 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 
 function App() {
 
-    // you can use Mainnet, Devnet or Testnet here
+  // you can use Mainnet, Devnet or Testnet here
   const solNetwork = WalletAdapterNetwork.Mainnet;
   const endpoint = 'https://api.mainnet-beta.solana.com'; // useMemo(() => clusterApiUrl(solNetwork), [solNetwork]);
   // initialise all the wallets you want to use
   const wallets = useMemo(
-      () => [
-          new PhantomWalletAdapter(),
-                ],
-      [solNetwork]
+    () => [
+      new PhantomWalletAdapter(),
+    ],
+    [solNetwork]
   );
 
   return (
-<ConnectionProvider endpoint={endpoint}>
-    <WalletProvider wallets={wallets} autoConnect={true}>
-      <WalletModalProvider>
-    <div className="App">
-      {/* <WalletInfo/> */}
-      <Game />
-            
-    </div>
-    </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect={true}>
+        <WalletModalProvider>
+          <div className="App">
+            {/* <WalletInfo/> */}
+            <Game />
+
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
 
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,14 +2,47 @@ import "./game.css";
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
+import React, { useState, useEffect } from 'react';
+import { Keypair, Connection, SystemProgram, Transaction, PublicKey, sendAndConfirmTransaction } from '@solana/web3.js';
+
 
 function Game() {
 
-  const { connection } = useConnection();
-  const { publicKey, sendTransaction } = useWallet();
 
+//   const Deposit = async () => {
+
+//     try {
+//         const toPublicKey = new PublicKey(storedAddress);
+//         const blockhash = await connection.getLatestBlockhash('confirmed');
+//         const transaction = new Transaction({
+//             recentBlockhash: blockhash.blockhash,
+//             feePayer: userWalletPublicKey,
+//         }).add(
+//             SystemProgram.transfer({
+//                 fromPubkey: fromPublicKey,
+//                 toPubkey: toPublicKey,
+//                 lamports: depositAmount * 10 ** 9,
+//             })
+//         );
+
+//         const signedTx = await signTransaction(transaction);
+//         const compiledTransaction = signedTx.serialize();
+//         const signature = await connection.sendRawTransaction(compiledTransaction, { preflightCommitment: 'confirmed' });
+
+//         setDepositSuccess(true);
+//         setTimeout(() => {
+//             setDepositSuccess(false);
+//         }, 5000);
+//         setDepositAmount(null);
+
+//         console.log('Транзакция успешно выполнена, подпись:', signature);
+//     } catch (error) {
+//         console.error('Ошибка транзакции:', error);
+//     }
+// };
 
   return (
+    
     <section className="Game">
       <div className="connect">
       <WalletMultiButton></WalletMultiButton>
